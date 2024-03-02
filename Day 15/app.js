1c47c78ccdb55dc98d9b5a704899c446ca234d45
Prakash singh <prakash2058.be22@chitkara.edu.in>
2024-03-02 20:15:28 +0530

    second

--- Day 15/app.js
+++ Day 15/app.js
@@ -1,3 +1,64 @@
-let pencilprice = 10;
-let output = `the total price is : ${pencilprice} Rupees.`;//This is template literals.
-console.log(output);
+// let pencilprice = 10;
+// let output = `the total price is : ${pencilprice} Rupees.`;//This is template literals.
+// console.log(output);
+
+
+//comparison for non number
+
+// uni code=unique code.every letter has its uni code.
+// a<b<c<d<e<f<g...AbortController;
+
+// conditional statement.
+// let age=23;
+// if(age>=18){
+//     console.log("you can vote and drive");
+// }
+// if(age<18){
+//     console.log("you cannot vote");
+// }
+
+// Truthy and Falsy.
+// Everything in JS is true or false (in boolean context)
+// This doesnot mean their value itself is false or true but they are treated as false or true if taken in boolean context.
+
+// Falsy values:-false,0,-0,0n(Bigint value),""(empty value),null,undefined,NaN;
+
+// Truthy values :-Everythingn else;
+
+// if(true){
+//     console.log("It has ttrue value");
+// }else{
+//     console.log("It has false value");
+// }
+
+// switch statement.
+ 
+// let color="red";
+// switch(color){
+//     case "red"
+//     console.log("stop");
+//     case "yellow"
+//     console.log("slow down");
+//     case "green"
+//     console.log("GO");
+//     break;
+    
+// }
+
+// Alert and prompt.
+
+// Alert display an alert message on the page 
+// alert("Something is wrong");
+
+//Prompt display a dialog box that ask user for some input.
+// prompt("Please enter your roll no.");
+// console.error("This is an error");
+// console.warn("This is an warning"); 
+
+// let firstname=prompt("Enter your first name");
+// let lastname=prompt("Enter your last name");
+// console.log("Welcome",firstname," ",lastname,"!");
+
+//STRING METHOD..
+formate
+stringName.method();
---
+++ Day 15/app1.js
@@ -0,0 +1,32 @@
+
+// switch statement.
+ 
+// let color="red";
+// switch(color){
+//     case "red"
+//     console.log("stop");
+//     case "yellow"
+//     console.log("slow down");
+//     case "green"
+//     console.log("GO");
+//     break;
+    
+// }
+
+// Alert and prompt.
+
+// Alert display an alert message on the page 
+// alert("Something is wrong");
+
+//Prompt display a dialog box that ask user for some input.
+// prompt("Please enter your roll no.");
+// console.error("This is an error");
+// console.warn("This is an warning"); 
+
+// let firstname=prompt("Enter your first name");
+// let lastname=prompt("Enter your last name");
+// console.log("Welcome",firstname," ",lastname,"!");
+
+//STRING METHOD..
+formate
+stringName.method();
